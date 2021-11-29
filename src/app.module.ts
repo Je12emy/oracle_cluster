438475c1145089f4de175726918ae81c8a385967
Je12emy <jeremyzelaya@hotmail.es>
2021-11-28 18:53:21 -0600

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { EmployeesModule } from './employees/employees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: "oracle",
+      host: "",
+      port: 123,
+      username: "sys",
+      password: "root",
+      database: "orcl",
+      entities: [],
+      synchronize: false
+    }),
     GraphQLModule.forRoot({
       debug: true,
       playground: true
