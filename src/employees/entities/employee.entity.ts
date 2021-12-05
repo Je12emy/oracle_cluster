4877c8ce2f6d10ffbaac7284d6c3fc43ccf031b5
Je12emy <jeremyzelaya@hotmail.es>
2021-12-05 00:49:55 -0600

    Update employees entity

--- src/employees/entities/employee.entity.ts
+++ src/employees/entities/employee.entity.ts
@@ -1,4 +1,4 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, Float } from '@nestjs/graphql';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity("EMPLOYEES")
@@ -9,11 +9,11 @@ export class Employee {
   employeeId: number;
 
   @Column({ name: "FIRST_NAME", type: "varchar2", length: 20, nullable: true })
-  @Field(() => String)
-  firstName: string;
+  @Field(() => String, { nullable: true })
+  firstName?: string;
 
   @Column({ name: "LAST_NAME", type: "varchar2", length: 25, nullable: true })
-  @Field(() => String)
+  @Field(() => String )
   lastName: string;
   
   @Column({ name: "EMAIL", type: "varchar2", length: 25, nullable: false })
@@ -21,8 +21,8 @@ export class Employee {
   email: string;
   
   @Column({ name: "PHONE_NUMBER", type: "varchar2", length: 20, nullable: true })
-  @Field(() => Int)
-  phoneNumber: number;
+  @Field(() => String, { nullable: true })
+  phoneNumber?: string;
 
   @Column({ name: "HIRE_DATE", type: "date", nullable: true })
   @Field(() => String)
@@ -33,18 +33,18 @@ export class Employee {
   jobId: number;
 
   @Column({ name: "SALARY", type: "number", nullable: false, precision: 2 })
-  @Field(() => Int)
-  salary: number;
+  @Field(() => Float, { nullable: true })
+  salary?: number;
 
-  @Column({ name: "COMMISSION_PCT", type: "number", nullable: false, precision: 2 })
-  @Field(() => Int)
-  commisionPct: number;
+  @Column({ name: "COMMISSION_PCT", type: "number", nullable: true, precision: 2 })
+  @Field({ nullable: true })
+  commissionPct?: number;
 
   @Column({ name: "MANAGER_ID", type: "number", nullable: false })
-  @Field(() => Int)
-  managerId: number;
+  @Field(() => Int, { nullable: true })
+  managerId?: number;
 
   @Column({ name: "DEPARTMENT_ID", type: "number", nullable: false })
-  @Field(() => Int)
-  departmentId: number;
+  @Field(() => Int, { nullable: true })
+  departmentId?: number;
 }
